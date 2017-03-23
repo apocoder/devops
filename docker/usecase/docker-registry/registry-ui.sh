0866a5c3f1e323f7955a6abdd6fabafa6f9677f5
root <apocoder@gmail.com>
2017-03-23 11:14:48 +0300

    added drdashboard

--- docker/usecase/docker-registry/registry-ui.sh
+++ docker/usecase/docker-registry/registry-ui.sh
@@ -4,3 +4,5 @@ sudo docker run \
   -e ENV_DOCKER_REGISTRY_PORT=5000 \
   -p 3000:80 \
   konradkleine/docker-registry-frontend:v2
+
+  docker run -it -p 3000:3000 --name registry-web --link registry -e REGISTRY_URL=http://registry-srv:5000/v2 -e REGISTRY_NAME=localhost:5000 hyper/docker-registry-web 
