5655f1b0e27ee58dff6564be86493b12b08e0887
Abdullah <apocoder@gmail.com>
2017-03-29 16:51:28 +0300

    added mesos compose

--- docker/usecase/docker-registry/registry-ui.sh
+++ docker/usecase/docker-registry/registry-ui.sh
@@ -7,3 +7,8 @@ sudo docker run \
 
   docker run -it -p 3000:3000 --name registry-web -e REGISTRY_NAME=localhost REGISTRY_HOST=172.17.0.1 REGISTRY_PORT=5000 hyper/docker-registry-web 
   docker run -p 3000:3000 -e REG1=http://172.17.0.1:5000/v2/  atcol/docker-registry-ui
+
+  docker run -d -p 3000:3000 -e REG1=http://172.17.0.1:5000/v1/ -e REG2=http://prod/v1/ atcol/docker-registry-ui
+
+  curl -Ls https://github.com/mkuchin/docker-registry-web/releases/download/v0.1.2/examples.tar.gz | tar -xzv
+cd examples/auth-enabled/
--- docker/usecase/jenkins-ci-cd/nodejs_app/build_and_run.sh
+++ docker/usecase/jenkins-ci-cd/nodejs_app/build_and_run.sh
@@ -1,2 +1,8 @@
 docker build -t apnodejs_image .
 docker run -p 8000:8000 apnodejs_image
+
+# Build an image
+$ docker build -t localhost:5000/apocoder/nodejs_app .
+
+# Push it to the registry
+$ docker push localhost:5000/apocoder/nodejs_app
